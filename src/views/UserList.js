19847f2a02e15e20e2423d5a70af75c146742e4a
luisgs7 <luisgsilva270@gmail.com>
2024-06-29 16:24:23 -0300

    criado a lista de usuários.

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,7 +1,36 @@
 import React from "react"
-import { Text } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
+import users from '../data/users'
+import { ListItem } from '@rneui/themed'
+import { Avatar } from "react-native-elements"
+import { ListItemContent } from "@rneui/base/dist/ListItem/ListItem.Content"
 
+export default props => {
+    console.warn(props)
 
-export default props => (
-    <Text>UserList</Text>
-)
+    function getUserItem({item: user}) {
+        return (
+            <ListItem bottomDivider
+            onPress={() => props.navigation.navigate('UserForm')}
+            >
+                <Avatar
+                    rounded
+                    source={{uri: user.avatarUrl}}
+                />
+                <ListItemContent>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                </ListItemContent>
+            </ListItem>
+        )
+    }
+    return (
+        <View>
+            <FlatList 
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+            />
+        </View>
+    )
+}
