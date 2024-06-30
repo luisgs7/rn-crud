5953c383648205a9929b7cf4454b4cd0ffe96d98
luisgs7 <luisgsilva270@gmail.com>
2024-06-29 22:09:09 -0300

    adicionado os icones de ação.

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,17 +1,48 @@
 import React from "react"
-import { FlatList, Text, View } from 'react-native'
+import { Alert, FlatList, Text, View } from 'react-native'
 import users from '../data/users'
 import { ListItem } from '@rneui/themed'
-import { Avatar } from "react-native-elements"
+import { Avatar, Button, Icon } from "react-native-elements"
 import { ListItemContent } from "@rneui/base/dist/ListItem/ListItem.Content"
 
 export default props => {
     console.warn(props)
 
+    function confirmUserDeletion(user) {
+        Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+            {
+                text: 'Sim',
+                onPress() {
+                    console.warn('delete ' + user.id)
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
+
+    function getActions(user) {
+        return (
+        <>
+            <Button
+                onPress={() => props.navigation.navigate('UserForm', user)}
+                type="clear"
+                icon={<Icon name="edit" size={25} color="orange"/>}
+            />
+            <Button
+                onPress={() => confirmUserDeletion(user)}
+                type="clear"
+                icon={<Icon name="delete" size={25} color="red"/>}
+            />
+        </>
+        )
+    }
+
     function getUserItem({item: user}) {
         return (
             <ListItem bottomDivider
-            onPress={() => props.navigation.navigate('UserForm')}
+            onPress={() => props.navigation.navigate('UserForm', user)}
             >
                 <Avatar
                     rounded
@@ -21,6 +52,12 @@ export default props => {
                     <ListItem.Title>{user.name}</ListItem.Title>
                     <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
                 </ListItemContent>
+
+                <ListItem.Content right>
+                    <ListItem.Title right>
+                        {getActions(user)}
+                    </ListItem.Title>
+                </ListItem.Content>
             </ListItem>
         )
     }
