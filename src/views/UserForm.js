5ffc5990a4a957114213ee629fb840628be890da
luisgs7 <luisgsilva270@gmail.com>
2024-06-30 11:15:00 -0300

    useState hook

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,7 +1,51 @@
-import React from "react"
-import { Text } from 'react-native'
+import React, { useState } from "react"
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { Button } from "react-native-elements"
 
 
-export default props => (
-    <Text>UserForm</Text>
-)
+export default ({route, navigation}) => {
+    const [user, setUser] = useState(route.params ? route.params : {})
+    return (
+        <View style={style.form}>
+            <Text>Nome</Text>
+            <TextInput 
+                style={style.input}
+                onChangeText={name => setUser({...user, name})}
+                placeholder="Informe o Nome"
+                value={user.name}
+            />
+            <Text>Email</Text>
+            <TextInput 
+                style={style.input}
+                onChangeText={email => setUser({...user, email})}
+                placeholder="Informe o E-mail"
+                value={user.email}
+            />
+            <Text>URL do Avatar</Text>
+            <TextInput 
+                style={style.input}
+                onChangeText={avatarUrl => setUser({...user, avatarUrl})}
+                placeholder="Informe o AvatarURL"
+                value={user.avatarUrl}
+            />
+            <Button
+                title="Salvar"
+                onPress={() => {
+                    navigation.goBack()
+                }}
+            />
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    form: {
+        padding: 12,
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 15,
+    }
+})
